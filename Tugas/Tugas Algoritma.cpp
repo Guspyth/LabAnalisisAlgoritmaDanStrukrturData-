9ec98163548ea6cc13301696043ef347258e4afd
Guspyth <agusnurfajri04@gmail.com>
2025-03-17 14:53:07 +0700

    First commit

---
+++ Tugas/Tugas Algoritma.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void insertionSort(string arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        string key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) { 
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int jumlahBuku;
+    cout << "Masukkan jumlah buku: ";
+    cin >> jumlahBuku;
+    cin.ignore();
+
+    string *judulBuku = new string[jumlahBuku];
+
+    cout << "Masukkan judul buku (1 baris per buku):" << endl;
+    for (int i = 0; i < jumlahBuku; i++) {
+        getline(cin, judulBuku[i]);
+    }
+
+    insertionSort(judulBuku, jumlahBuku);
+
+    cout << "\nDaftar buku setelah diurutkan (Ascending A-Z):" << endl;
+    for (int i = 0; i < jumlahBuku; i++) {
+        cout << judulBuku[i] << endl;
+    }
+
+    delete[] judulBuku;
+    return 0;
+}
+
